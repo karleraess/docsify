f1d2446cb3f98fced5f4e863ec03901af3dadfac
gongjiu <gongjiu.zhang@tuya.com>
2023-04-14 10:09:44 +0800

    init

--- docs/body.js
+++ docs/body.js
@@ -4,4 +4,4 @@ document.writeln("<script src=\'./src/docsify.min.js\'></script>	");
 //自定义js在后
 document.writeln("<script src=\'./src/search.js\'></script>");
 document.writeln("<script src=\'./src/mermaid.min.js\'></script>");
-document.writeln("<script src=\'./src/docsify-plantuml.min.js\'></script>");
+//document.writeln("<script src=\'./src/docsify-plantuml.min.js\'></script>");
