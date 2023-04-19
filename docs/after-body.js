4a790726bd823333aee587e46f65011b9a7feebc
gongjiu <gongjiu.zhang@tuya.com>
2023-04-19 14:28:02 +0800

    剪切板

---
+++ docs/after-body.js
@@ -0,0 +1,2 @@
+document.writeln("<script src=\'./src/docsify-copy-code\'></script>");
+//document.writeln("<script src=\'./src/docsify-plantuml.min.js\'></script>");
--- docs/body.js
+++ docs/body.js
@@ -2,5 +2,4 @@ document.writeln("<script src=\'./src/docsify.min.js\'></script>	");
 
 document.writeln("<script src=\'./src/search.js\'></script>");
 document.writeln("<script src=\'./src/mermaid.min.js\'></script>");
-document.writeln("<script src=\'./src/docsify-copy-code.min.js\'></script>");
 //document.writeln("<script src=\'./src/docsify-plantuml.min.js\'></script>");
