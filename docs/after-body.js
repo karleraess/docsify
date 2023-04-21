c187b26829959b73bcfa202654f92896d5b2614e
gongjiu <gongjiu.zhang@tuya.com>
2023-04-21 11:27:11 +0800

    目录

--- docs/after-body.js
+++ docs/after-body.js
@@ -1,1 +1,2 @@
 document.writeln("<script src=\'./src/docsify-copy-code.js\'></script>");
+document.writeln("<script src=\'./src/toc.js\'></script>");
--- docs/header.js
+++ docs/header.js
@@ -1,2 +1,3 @@
 document.writeln("<link rel=\'stylesheet\' href=\'./src/vue.css\' />");
 document.writeln("<link rel=\'stylesheet\' href=\'./src/mermaid.min.css\' />");
+document.writeln("<link rel=\'stylesheet\' href=\'./src/toc.css\' />");
